af2a74a8220325b4d538b10e78074ad589175cb2
Blair Bristow <blairhbristow@gmail.com>
2022-04-22 12:27:02 -0400

    Add function to add items to the table

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-let dataSource = [
-    {name: '+5 Dexterity Vest', SellIn: '10', Quality: '20', Date: '4/22/2022'},
-    {name: 'Aged Bri', SellIn: '2', Quality: '0', Date: '4/22/2022'},
-    {name: 'Elixir of the Mongoose', SellIn: '5', Quality: '7', Date: '4/22/2022'},
-    {name: 'Sulfuras, Hand of Ragnaros', SellIn: '0', Quality: '80', Date: '4/22/2022'},
-    {name: 'Backstage passes to a TAFKAL80ETC concert', SellIn: '15', Quality: '20', Date: '4/22/2022'},
-    {name: 'Conjered Mana Cake', SellIn: '3', Quality: '6', Date: '4/22/2022'}
+const dataSource = [
+    { name: '+5 Dexterity Vest', SellIn: '10', Quality: '20', Date: '4/22/2022' },
+    { name: 'Aged Bri', SellIn: '2', Quality: '0', Date: '4/22/2022' },
+    { name: 'Elixir of the Mongoose', SellIn: '5', Quality: '7', Date: '4/22/2022' },
+    { name: 'Sulfuras, Hand of Ragnaros', SellIn: '0', Quality: '80', Date: '4/22/2022' },
+    { name: 'Backstage passes to a TAFKAL80ETC concert', SellIn: '15', Quality: '20', Date: '4/22/2022' },
+    { name: 'Conjered Mana Cake', SellIn: '3', Quality: '6', Date: '4/22/2022' }
 ]
+
+const input = document.querySelectorAll('input')
+
+function addItem(event) {
+    event.preventDefault()
+    const data = new FormData(event.target)
+    const value = Object.fromEntries(data.entries())
+    const objectValue = ({ value })
+    const trueObject = objectValue.value
+    const dataTable = $('#dt').DataTable()
+    dataTable.row.add(trueObject).draw()
+}
+
+const form = document.querySelector('.item-add');
+
+form.addEventListener('submit', addItem);
 
 $(document).ready(function () {
     createTable()
