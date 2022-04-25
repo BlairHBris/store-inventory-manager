cc9cee10297fe03413202e8b8816831fb776ee26
Blair Bristow <blairhbristow@gmail.com>
2022-04-25 10:25:22 -0400

    Add styling

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const dataSource = [
-    { name: '+5 Dexterity Vest', SellIn: '10', Quality: '20', Date: '4/22/2022' },
-    { name: 'Aged Bri', SellIn: '2', Quality: '0', Date: '4/22/2022' },
-    { name: 'Elixir of the Mongoose', SellIn: '5', Quality: '7', Date: '4/22/2022' },
-    { name: 'Sulfuras, Hand of Ragnaros', SellIn: '0', Quality: '80', Date: '4/22/2022' },
-    { name: 'Backstage passes to a TAFKAL80ETC concert', SellIn: '15', Quality: '20', Date: '4/22/2022' },
-    { name: 'Conjered Mana Cake', SellIn: '3', Quality: '6', Date: '4/22/2022' }
+    { name: '+5 Dexterity Vest', SellIn: '10', Quality: '20', Date: '04/17/2022' },
+    { name: 'Aged Bri', SellIn: '2', Quality: '0', Date: '04/17/2022' },
+    { name: 'Elixir of the Mongoose', SellIn: '5', Quality: '7', Date: '04/17/2022' },
+    { name: 'Sulfuras, Hand of Ragnaros', SellIn: '0', Quality: '80', Date: '04/17/2022' },
+    { name: 'Backstage passes to a TAFKAL80ETC concert', SellIn: '15', Quality: '20', Date: '04/17/2022' },
+    { name: 'Conjered Mana Cake', SellIn: '3', Quality: '6', Date: '04/17/2022' }
 ]
-
-const input = document.querySelectorAll('input')
 
 function addItem(event) {
     event.preventDefault()
@@ -15,6 +13,7 @@ function addItem(event) {
     const value = Object.fromEntries(data.entries())
     const objectValue = ({ value })
     const trueObject = objectValue.value
+    dataSource.push(trueObject)
     const dataTable = $('#dt').DataTable()
     dataTable.row.add(trueObject).draw()
 }
@@ -22,6 +21,23 @@ function addItem(event) {
 const form = document.querySelector('.item-add');
 
 form.addEventListener('submit', addItem);
+
+function qualityCalculation(event) {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const date = formData.get('Current Date')
+    console.log(date)
+    console.log(dataSource)
+    /*const objectValue = ({ value })
+    const trueObject = objectValue.value
+    const dataTable = $('#dt').DataTable()
+    dataTable.column.add(trueObject).draw()*/
+}
+
+const submitDate = document.querySelector('.submit-date')
+
+submitDate.addEventListener('submit', qualityCalculation);
+
 
 $(document).ready(function () {
     createTable()
@@ -39,7 +55,8 @@ function createTable() {
             { data: 'name' },
             { data: 'SellIn' },
             { data: 'Quality' },
-            { data: 'Date' }
+            { data: 'Date' },
+            { defaultContent: 'Awaiting Calculation'}
         ],
 
         "paging": true,
@@ -52,4 +69,4 @@ function createTable() {
             return nRow;
         },
     })
-}  
+} 
