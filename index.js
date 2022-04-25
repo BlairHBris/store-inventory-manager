d470bd7038ef62d8da890258177205320a3ecef3
Blair Bristow <blairhbristow@gmail.com>
2022-04-25 13:08:09 -0400

    Restructure

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const dataSource = [
-    { name: '+5 Dexterity Vest', SellIn: '10', Quality: '20', Date: '04/17/2022' },
-    { name: 'Aged Bri', SellIn: '2', Quality: '0', Date: '04/17/2022' },
-    { name: 'Elixir of the Mongoose', SellIn: '5', Quality: '7', Date: '04/17/2022' },
-    { name: 'Sulfuras, Hand of Ragnaros', SellIn: '0', Quality: '80', Date: '04/17/2022' },
-    { name: 'Backstage passes to a TAFKAL80ETC concert', SellIn: '15', Quality: '20', Date: '04/17/2022' },
-    { name: 'Conjered Mana Cake', SellIn: '3', Quality: '6', Date: '04/17/2022' }
+const items = [
+    { name: '+5 Dexterity Vest', SellIn: '10', Quality: '20', Date: '04/20/2022' },
+    { name: 'Aged Bri', SellIn: '2', Quality: '0', Date: '04/21/2022' },
+    { name: 'Elixir of the Mongoose', SellIn: '5', Quality: '7', Date: '04/22/2022' },
+    { name: 'Sulfuras, Hand of Ragnaros', SellIn: '0', Quality: '80', Date: '04/23/2022' },
+    { name: 'Backstage passes to a TAFKAL80ETC concert', SellIn: '15', Quality: '20', Date: '04/19/2022' },
+    { name: 'Conjered Mana Cake', SellIn: '3', Quality: '6', Date: '04/24/2022' }
 ]
 
 function addItem(event) {
@@ -13,60 +13,56 @@ function addItem(event) {
     const value = Object.fromEntries(data.entries())
     const objectValue = ({ value })
     const trueObject = objectValue.value
-    dataSource.push(trueObject)
-    const dataTable = $('#dt').DataTable()
-    dataTable.row.add(trueObject).draw()
+    items.push(trueObject)
 }
 
-const form = document.querySelector('.item-add');
+const form = document.querySelector('.item-add')
+form.addEventListener('submit', addItem)
 
-form.addEventListener('submit', addItem);
+
+const generate = document.querySelector('.generate')
+const tbody = document.querySelector('tbody')
+
+function addTable (item) {
+    const tr = document.createElement("tr")
+    tr.innerHTML = `
+    <td>${item.name}</td>
+    <td>${item.SellIn}</td>
+    <td>${item.Quality}</td>
+    <td>${item.Date}</td>
+    <td>${item.RemSellIn}</td>
+    <td>${item.CurrQuality}</td>
+    `
+    tbody.append(tr)
+}
+
+const table = document.querySelector('.table')
+
+generate.addEventListener('click', event => {
+    tbody.innerHTML=``
+    table.style.display = "block"
+    items.forEach(item => {
+        addTable(item) 
+    })
+})
+
+currentDate = document.querySelector('.current-date')
 
 function qualityCalculation(event) {
     event.preventDefault()
     const formData = new FormData(event.target)
     const date = formData.get('Current Date')
-    console.log(date)
-    console.log(dataSource)
-    /*const objectValue = ({ value })
-    const trueObject = objectValue.value
-    const dataTable = $('#dt').DataTable()
-    dataTable.column.add(trueObject).draw()*/
+    tbody.innerHTML=``
+    items.forEach(item => {
+        const date1 = new Date(date)
+        const date2 = new Date(item.Date)
+        const timeDifference = date1.getTime() -date2.getTime()
+        const dayDifference = timeDifference / (1000*3600*24)
+        item['RemSellIn'] = item.SellIn - dayDifference
+        item['CurrQuality'] = item.Quality - dayDifference
+        addTable(item)
+    })
 }
 
 const submitDate = document.querySelector('.submit-date')
-
-submitDate.addEventListener('submit', qualityCalculation);
-
-
-$(document).ready(function () {
-    createTable()
-})
-function createTable() {
-    $('#dt').DataTable({
-        dom: 'Bfrtip',
-        data: dataSource,
-        columns: [
-            {
-                render: function (data, type, row, meta) {
-                    return meta.row + meta.settings._iDisplayStart + 1;
-                }
-            },
-            { data: 'name' },
-            { data: 'SellIn' },
-            { data: 'Quality' },
-            { data: 'Date' },
-            { defaultContent: 'Awaiting Calculation'}
-        ],
-
-        "paging": true,
-        "info": true,
-        "language": {
-            "emptyTable": "No data available"
-        },
-        "fnRowCallback": function (nRow, aData, iDisplayIndex) {
-            $("td:first", nRow).html(iDisplayIndex + 1);
-            return nRow;
-        },
-    })
-} 
+submitDate.addEventListener('submit', qualityCalculation)
