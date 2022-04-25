1555ce72e75f8d063dc3945a900bca33e6868a68
Blair Bristow <blairhbristow@gmail.com>
2022-04-25 15:56:36 -0400

    Add full quality calculation)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const items = [
     { name: 'Elixir of the Mongoose', SellIn: '5', Quality: '7', Date: '04/22/2022' },
     { name: 'Sulfuras, Hand of Ragnaros', SellIn: '0', Quality: '80', Date: '04/23/2022' },
     { name: 'Backstage passes to a TAFKAL80ETC concert', SellIn: '15', Quality: '20', Date: '04/19/2022' },
-    { name: 'Conjered Mana Cake', SellIn: '3', Quality: '6', Date: '04/24/2022' }
+    { name: 'Conjured Mana Cake', SellIn: '3', Quality: '6', Date: '04/24/2022' }
 ]
 
 function addItem(event) {
@@ -63,8 +63,24 @@ function qualityCalculation(event) {
             item['CurrQuality'] = Number(item.Quality) + dayDifference
         } else if (item.name.includes('Sulfuras')) {
             item['CurrQuality'] = item.Quality
+        }else if (item.name.includes('Backstage passes')) {
+            if (Number(item.RemSellIn) > 10) {
+                item['CurrQuality'] = Number(item.Quality) + dayDifference
+            } else if (Number(item.RemSellIn) <= 10 && Number(item.RemSellIn) > 5) {
+                item['CurrQuality'] = Number(item.Quality) + (Number(item.SellIn) - 10) + (2 * (10 - Number(item.RemSellIn)))
+            } else if (Number(item.RemSellIn) <= 5) {
+                item['CurrQuality'] = Number(item.Quality) + (Number(item.SellIn) - 10) + 10 + (3 * (5 - Number(item.RemSellIn)))
+            }
+            if (Number(item.RemSellIn) <= 0) {
+                item['CurrQuality'] = 0
+            }
+        } else if (item.name.includes('Conjured')) {
+            item['CurrQuality'] = item.Quality - (2 * Number(dayDifference))
         } else {
             item['CurrQuality'] = item.Quality - dayDifference
+        }
+        if (Number(item.CurrQuality) >= 50 && !item.name.includes('Sulfuras')) {
+            item.CurrQuality = 50
         }
         addTable(item)
     })
